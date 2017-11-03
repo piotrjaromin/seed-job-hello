b33f5550a430eebbe8467602441ea17192f8c28b
Piotr <piotr@Administrators-MacBook-Pro.local>
2017-11-03 15:48:19 +0100

    added groovy files

---
+++ seed_jobs/default.groovy
@@ -0,0 +1,33 @@
+import groovy.json.JsonSlurper
+
+def configFile = new File("../config/pipelines.json")
+def config = new JsonSlurper().parseText(configFile.text)
+
+
+config.pipelines.each {
+
+    job("${it.name}-${it.branch}-build") {
+        description "Building the ${it.branch} branch."
+        parameters {
+            stringParam('COMMIT', 'HEAD', 'Commit to build')
+        }
+        scm {
+            git {
+                remote {
+                    url '${config.gitPrefix}/${it.git}'
+                    branch '${COMMIT}'
+                }
+                extensions {
+                    wipeOutWorkspace()
+                    localBranch it.branch
+                }
+            }
+        }
+        triggers {
+            scm('H/15 * * * *')
+        }
+        steps {
+            shell "Look! I'm building ${BRANCH}!"
+        }
+    }
+}

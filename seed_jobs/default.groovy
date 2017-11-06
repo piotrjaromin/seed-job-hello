c4e6576fa0221e763c230994bd91ff34c16aa8b3
Piotr <piotr@Administrators-MacBook-Pro.local>
2017-11-06 10:47:44 +0100

    test

--- seed_jobs/default.groovy
+++ seed_jobs/default.groovy
@@ -7,32 +7,25 @@ def config = new JsonSlurper().parseText(configFile.text)
 config.pipelines.each { pipeline ->
 
     pipelineJob("${pipeline.name}-${pipeline.branch}-build") {
+        description "Building the ${pipeline.branch} branch."
         parameters {
             stringParam('COMMIT', 'HEAD', 'Commit to build')
         }
         definition {
-            cps {
-                    script "Jenkinsfile"
+            cpsScm {
+                scm {
+                    git {
+                        branch pipeline.branch
+                        remote {
+                            url "${config.gitPrefix}/${pipeline.git}"
+                        }
+                    }
                 }
-        }
-        description "Building the ${pipeline.branch} branch."
-        scm {
-            git {
-                remote {
-                    url "${config.gitPrefix}/${pipeline.git}"
-                    branch pipeline.branch
-                }
-                extensions {
-                    wipeOutWorkspace()
-                    localBranch pipeline.branch
-                }
+                scriptPath "Jenkinsfile"
             }
         }
         triggers {
             scm('H/15 * * * *')
-        }
-        steps {
-            shell "Look! I'm building!"
         }
     }
 }
